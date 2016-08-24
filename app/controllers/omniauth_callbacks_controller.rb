5d373aa98e5b0f2cc5b053206b03e3bd9f06fac2
Blackhat Digital <josh@blackhatdigital.com.au>
2016-08-25 07:11:23 +1000

    remove flash message from stripe

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -9,7 +9,6 @@ class OmniauthCallbacksController < ApplicationController
     })
       # anything else you need to do in response..
       sign_in_and_redirect @user, :event => :authentication
-      set_flash_message(:notice, :success, :kind => "Stripe") if is_navigational_format?
     else
       session["devise.stripe_connect_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
